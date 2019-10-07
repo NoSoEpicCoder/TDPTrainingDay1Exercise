b2b454c379f0e3a1c82f29b70aba6f991cbb6fd6
Gilbert <kevin.gilbert@accenture.com>
2019-10-07 15:25:39 -0400

    First commit

---
+++ src/StringExercise.java
@@ -0,0 +1,65 @@
+public class StringExercise {
+
+    public static void main(String[] args){
+        //9.2.1
+        String s = "Amanda Chui";
+        System.out.println(s.charAt(3)); //n
+        System.out.println(s.substring(8)); //hui
+        System.out.println(s.length()); //11
+        System.out.println(s.indexOf('a')); //2
+        System.out.println(s.charAt(0)); //A
+        System.out.println(s.substring(1, 4)); //man
+        System.out.println(s.substring(1)); //manda Chui
+        System.out.println(s.indexOf('m', 4)); //-1
+        System.out.println(s.charAt(4)); //d
+        System.out.println(s.substring(4, 5)); //d
+
+        //9.2.3
+        String name = "Avi Laurie";
+        System.out.println(name.toLowerCase().indexOf('a')); //0
+        System.out.println(name.toUpperCase().charAt(5)); //A
+        System.out.println(name.substring(3).indexOf('i')); //5
+        System.out.println(name.substring(2).toUpperCase()); //I LAURIE
+        System.out.println(name.toUpperCase().indexOf('A', 1)); //5
+        System.out.println(name.substring(name.indexOf(' ')+1).length()); //6
+
+        //Methods
+        System.out.println(count('l', "hello world")); //l appears 3 times
+        System.out.println(replace("bonjour", 'o', '0')); //will return b0nj0ur
+        System.out.println(isHeteroPair("TERRY", "FOX")); //will return true that these two are a hetero pair
+    }
+
+    //9.2.4
+    public static int count(char c, String s){
+        int counter = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == c){
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    //9.2.5
+    public static String replace(String s, char oldChar, char newChar){
+        StringBuilder newString = new StringBuilder(s);
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == oldChar){
+                newString.setCharAt(i, newChar);
+            }
+        }
+        return newString.toString();
+    }
+
+    //9.2.7
+    public static boolean isHeteroPair(String wordA, String wordB){
+        for(int i=0; i<wordA.length(); i++){
+            for(int j=0; j<wordB.length(); j++){
+                if(wordA.charAt(i) == wordB.charAt(j)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ src/StringExerciseTest.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringExerciseTest {
+
+    @org.junit.jupiter.api.Test
+    void testCount() {
+        assertEquals(3, StringExercise.count('l', "Hello world"));
+        assertEquals(0, StringExercise.count('a', "Testing testing"));
+        assertEquals(2, StringExercise.count('e', "Gre@t exampl3"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testReplace() {
+        assertEquals("b0nj0ur", StringExercise.replace("bonjour", 'o', '0'));
+        assertEquals("aaaa - tvtvt - ssss", StringExercise.replace("aaaa - twtwt - ssss", 'w', 'v'));
+        assertEquals("John_Doe", StringExercise.replace("John Doe", ' ', '_'));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testIsHeteroPair() {
+        assertTrue(StringExercise.isHeteroPair("TERRY", "FOX"));
+        assertTrue(StringExercise.isHeteroPair("ROB", "KYLE"));
+        assertFalse(StringExercise.isHeteroPair("JACK", "BALL"));
+        assertFalse(StringExercise.isHeteroPair("WAYNE", "GRETZKY"));
+    }
+}
